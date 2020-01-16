b81ee00ba4747e11c1446cb94532ad3dca2b2950
tedyblood <tedy.panama@gmail.com>
2020-01-16 08:23:41 -0500

    Commit Inicial Archivo Funcional

---
+++ sendemail.php
@@ -0,0 +1,42 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "Peritaje" );
+define( "RECIPIENT_EMAIL", "tedy.panama@gmail.com" );
+
+// Read the form values
+$success = false;
+$name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['nombre'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$services = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
+$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$website = isset( $_POST['website'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['msg'] ) : "";
+
+$mail_subject = 'Elix Template: A Mail send by ' . $name;
+
+$body = 'Name: '. $name . "\r\n";
+$body .= 'Email: '. $senderEmail . "\r\n";
+
+
+if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
+if ($services) {$body .= 'services: '. $services . "\r\n"; }
+if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
+if ($website) {$body .= 'Website: '. $website . "\r\n"; }
+
+$body .= 'message: ' . "\r\n" . $message;
+
+
+
+// If all values exist, send the email
+if ( $name && $senderEmail && $message ) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $name . " <" . $senderEmail . ">";  
+  $success = mail( $recipient, $mail_subject, $body, $headers );
+  echo json_encode('enviado');
+}else {
+	echo json_encode('error');
+}
+
+?>
